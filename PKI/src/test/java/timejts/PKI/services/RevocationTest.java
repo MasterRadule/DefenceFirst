201251347bdb2d2c2b586e9063034fad8271614d
nmikovic <mikovic.nm@gmail.com>
2020-04-07 14:55:19 +0200

    Unused comments

--- PKI/src/test/java/timejts/PKI/services/RevocationTest.java
+++ PKI/src/test/java/timejts/PKI/services/RevocationTest.java
@@ -1,33 +1,10 @@
 package timejts.PKI.services;
 
-/*
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class EmailServiceTests {
-    @Autowired
-    private EmailService emailService;
-
-    @Test()
-    void sendEmail_emailSent() {
-        String to = "mikovic.nm@gmail.com";
-        File certificate = new File("C:\\Users\\korisnik\\Desktop\\mikan.pem");
-        assertDoesNotThrow(() -> emailService.sendEmail(to, certificate));
-    }
-}
-
- */
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import timejts.PKI.exceptions.CertificateAlreadyRevokedException;
-import timejts.PKI.exceptions.NotExistingCertificateException;
 import timejts.PKI.repository.RevokedCertificatesRepository;
 
-import java.io.IOException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RevocationTest {
