9e0d86e42e9ae12f307d7d7472a5e790c65f77e1
Danijel Radulović <master.daca09@gmail.com>
2020-05-28 13:16:35 +0200

    Implement X509 authentication on SIEMCentre

--- SIEMCentre/src/main/java/timejts/SIEMCentre/configurations/SecurityConfig.java
+++ SIEMCentre/src/main/java/timejts/SIEMCentre/configurations/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .anyRequest().authenticated().and()
                 .x509()
                 .authenticationUserDetailsService(new X509AuthenticatedUserDetailsService());
