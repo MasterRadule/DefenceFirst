8bef15572868125abaac55c8f884a1f960995a3f
Dragan Ćulibrk <33953768+draganculibrk9@users.noreply.github.com>
2020-06-15 13:54:49 +0200

    Creat DoS logs

--- Simulator/logs_machine.py
+++ Simulator/logs_machine.py
@@ -1,6 +1,16 @@
+import logging
 from abc import ABC, abstractmethod
 from random import randint
 from time import sleep
+
+OS = ['Windows', 'Linux']
+HOSTNAME = ['defence-first.rs', 'defence-first.de', 'defence-first.ru']
+HOSTIP = ['78.218.236.218', '87.236.11.212', '54.147.165.86']
+SOURCEIP = ['163.189.141.53', '204.164.10.7', '213.166.160.236', '123.197.235.233', '77.28.21.14']
+FACILITY = ['KERN', 'USER', 'MAIL', 'DAEMON', 'AUTH', 'SYSLOG', 'LPR', 'NEWS',
+            'UUCP', 'CLOCK_DAEMON', 'AUTHPRIV', 'FTP', 'NTP', 'LOGAUDIT', 'LOGALERT',
+            'CRON', 'LOCAL0', 'LOCAL1', 'LOCAL2', 'LOCAL3', 'LOCAL4', 'LOCAL5', 'LOCAL6', 'LOCAL7']
+FORMAT = '%(asctime)s     %(hostname)s-%(os)s-%(hostip)s-%(sourceip)s     %(levelname)s-%(facility)s     %(message)s'
 
 
 class State(ABC):
@@ -11,14 +21,34 @@ class State(ABC):
 
 class DoSAttack(State):
     def run(self, context):
-        print("DOS")
+        d1 = {'hostname': HOSTNAME[0], 'os': OS[1], 'hostip': HOSTIP[0], 'sourceip': SOURCEIP[0],
+              'facility': FACILITY[1]}
+        d2 = {'hostname': HOSTNAME[0], 'os': OS[1], 'hostip': HOSTIP[0], 'sourceip': SOURCEIP[1],
+              'facility': FACILITY[1]}
+        d3 = {'hostname': HOSTNAME[0], 'os': OS[1], 'hostip': HOSTIP[0], 'sourceip': SOURCEIP[2],
+              'facility': FACILITY[1]}
+        d4 = {'hostname': HOSTNAME[0], 'os': OS[1], 'hostip': HOSTIP[0], 'sourceip': SOURCEIP[3],
+              'facility': FACILITY[1]}
+        d5 = {'hostname': HOSTNAME[0], 'os': OS[1], 'hostip': HOSTIP[0], 'sourceip': SOURCEIP[4],
+              'facility': FACILITY[1]}
+
+        for i in range(20):
+            context.logger.info('Requested resource index.html', extra=d1)
+            sleep(0.1)
+            context.logger.info('Requested resource index.html', extra=d2)
+            sleep(0.1)
+            context.logger.info('Requested resource index.html', extra=d3)
+            sleep(0.1)
+            context.logger.info('Requested resource index.html', extra=d4)
+            sleep(0.1)
+            context.logger.info('Requested resource index.html', extra=d5)
+
         context.state = NormalState()
 
 
 class NormalState(State):
     def run(self, context):
         print("Normal")
-
         rand = randint(1, 3)
 
         if rand == 1:
@@ -44,6 +74,11 @@ class AccountMisusage(State):
 class Context:
     def __init__(self):
         self.state = NormalState()
+        logging.basicConfig(format=FORMAT, datefmt='%Y-%m-%dT%H:%M:%S%z')
+        logger = logging.getLogger('simulator')
+        logger.setLevel(logging.INFO)
+
+        self.logger = logger
 
     def run(self):
         self.state.run(self)
